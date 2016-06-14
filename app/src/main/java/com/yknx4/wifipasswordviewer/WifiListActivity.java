419c587c5387fab99a8ff2e6642dab2226dc88ad
Alejandro Figueroa <yknx.4.b@gmail.com>
2016-06-14 10:36:24 -0500

    Add new library to handle permissions

--- app/src/main/java/com/yknx4/wifipasswordviewer/WifiListActivity.java
+++ app/src/main/java/com/yknx4/wifipasswordviewer/WifiListActivity.java
@@ -1,10 +1,13 @@
 package com.yknx4.wifipasswordviewer;
 
+import android.*;
+import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -23,9 +26,15 @@ import com.google.android.gms.ads.NativeExpressAdView;
 import net.hockeyapp.android.CrashManager;
 import net.hockeyapp.android.UpdateManager;
 
-public class WifiListActivity extends AppCompatActivity {
+import java.util.List;
+
+import pub.devrel.easypermissions.AfterPermissionGranted;
+import pub.devrel.easypermissions.EasyPermissions;
+
+public class WifiListActivity extends AppCompatActivity implements EasyPermissions.PermissionCallbacks {
 
     private static final String TAG = WifiListActivity.class.getSimpleName();
+    private static final int INTERNET_AND_STORAGE = 99;
     WifiController mWifis;
     private RecyclerView mRecyclerView;
     private WifiItemAdapter adapter;
@@ -39,28 +48,28 @@ public class WifiListActivity extends AppCompatActivity {
 
 
         AsyncTask<Context,Void,WifiController> task = new AsyncTask<Context,Void,WifiController>(){
-                @Override
-                protected WifiController doInBackground(Context[] params) {
-                    WifiController newWifiController = new WifiController(params[0]);
-                    newWifiController.init();
-                    return newWifiController;
-                }
+            @Override
+            protected WifiController doInBackground(Context[] params) {
+                WifiController newWifiController = new WifiController(params[0]);
+                newWifiController.init();
+                return newWifiController;
+            }
 
-                @Override
-                protected void onPostExecute(WifiController o) {
-                    super.onPostExecute(o);
-                    TextView t = (TextView) findViewById(R.id.txt_loading);
-                    if(t!=null) t.setVisibility(View.GONE);
-                    mWifis = o;
-                    adapter = new WifiItemAdapter(WifiListActivity.this, mWifis.getNetworks());
-                    mRecyclerView.setAdapter(adapter);
+            @Override
+            protected void onPostExecute(WifiController o) {
+                super.onPostExecute(o);
+                TextView t = (TextView) findViewById(R.id.txt_loading);
+                if(t!=null) t.setVisibility(View.GONE);
+                mWifis = o;
+                adapter = new WifiItemAdapter(WifiListActivity.this, mWifis.getNetworks());
+                mRecyclerView.setAdapter(adapter);
 
-                }
-            };
-            mRecyclerView = (RecyclerView) findViewById(R.id.rv_wifi);
-            mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
-            mRecyclerView.setClickable(true);
-            task.execute(this);
+            }
+        };
+        mRecyclerView = (RecyclerView) findViewById(R.id.rv_wifi);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mRecyclerView.setClickable(true);
+        task.execute(this);
 
 
 
@@ -74,7 +83,7 @@ public class WifiListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_wifi_list);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        setUpAds();
+        loadAdsWithPermissions();
         checkForCrashes();
 
 
@@ -89,13 +98,13 @@ public class WifiListActivity extends AppCompatActivity {
         if (ContextCompat.checkSelfPermission(this,
                 android.Manifest.permission.INTERNET)
                 != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this,
-                        new String[]{android.Manifest.permission.INTERNET},
-                        0);
+            ActivityCompat.requestPermissions(this,
+                    new String[]{android.Manifest.permission.INTERNET},
+                    0);
 
-                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
-                // app-defined int constant. The callback method gets the
-                // result of the request.
+            // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+            // app-defined int constant. The callback method gets the
+            // result of the request.
         }
     }
 
@@ -153,7 +162,7 @@ public class WifiListActivity extends AppCompatActivity {
         mInterstitialAd.setAdListener(new AdListener() {
             @Override
             public void onAdClosed() {
-               requestNewInterstitial();
+                requestNewInterstitial();
             }
 
             @Override
@@ -176,4 +185,42 @@ public class WifiListActivity extends AppCompatActivity {
         mInterstitialAd.loadAd(adRequest);
     }
 
+    @AfterPermissionGranted(INTERNET_AND_STORAGE)
+    private void loadAdsWithPermissions() {
+        String[] perms;
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.JELLY_BEAN) {
+            perms = new String[]{Manifest.permission.INTERNET, Manifest.permission.READ_EXTERNAL_STORAGE};
+        }
+        else
+        {
+            perms = new String[]{Manifest.permission.INTERNET};
+        }
+        if (EasyPermissions.hasPermissions(this, perms)) {
+            // Already have permission, do the thing
+            // ...
+            setUpAds();
+        } else {
+            // Do not have permissions, request them now
+            EasyPermissions.requestPermissions(this, getString(R.string.permissions_rationale),
+                    INTERNET_AND_STORAGE, perms);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+
+        // Forward results to EasyPermissions
+        EasyPermissions.onRequestPermissionsResult(requestCode, permissions, grantResults, this);
+    }
+
+    @Override
+    public void onPermissionsGranted(int requestCode, List<String> perms) {
+
+    }
+
+    @Override
+    public void onPermissionsDenied(int requestCode, List<String> perms) {
+
+    }
 }
